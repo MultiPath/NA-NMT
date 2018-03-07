d4f85b9de2dff3ee72c91b2ca41a6488149a83e5
Jiatao Gu <gujt10@mails.tsinghua.edu.cn>
2018-03-07 20:13:19 +0800

    add a new script for simultaneous translation

---
+++ scripts2/run_iwslt_test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+python ez_run.py \
+                --prefix [time] \
+                --gpu 1 \
+                --eval-every 500 \
+                --dataset iwslt \
+                --tensorboard \
+                --data_prefix "/data0/data/transformer_data/" \
+                --use_wo \
+                --share_embeddings \
+                --realtime \
+                --pretrained_from "02.25_08.34.iwslt_subword_278_507_5_2_0.079_746_uni_.pt" \
+                --debug
+                #--params base-t2t \
+                #--fast --use_alignment --diag --positional_attention \
