3bc15e031fd8fe9c8c4bf37c406da194018a4d86
Jiatao Gu <thomagram@gmail.com>
2018-04-08 23:32:09 +0800

    update scripts

--- scripts2/resume_wmt.sh
+++ scripts2/resume_wmt.sh
@@ -6,14 +6,15 @@ python ez_run.py \
                 --data_prefix "/data1/ywang/" \
                 --workspace_prefix "/data0/workspace/unitrans/" \
                 --load_vocab \
-                --dataset europarl1 \
+                --dataset europarl2 \
                 --tensorboard \
                 --batch_size 1250 \
                 --inter_size 4 \
                 --use_wo \
                 --language roen \
-                #--debug
-                # --universal \
+                --universal \
+                --load_from "04.06_18.54.europarl2_subword_512_512_6_8_0.100_16000_" \
+                --resume \
                 # --debug \
                 # --load_dataset \
                 # --debug \
--- scripts2/run_wmt.sh
+++ scripts2/run_wmt.sh
@@ -6,15 +6,17 @@ python ez_run.py \
                 --data_prefix "/data1/ywang/" \
                 --workspace_prefix "/data0/workspace/unitrans/" \
                 --load_vocab \
-                --dataset europarl2 \
+                --dataset europarl_6k_fv \
                 --tensorboard \
                 --batch_size 1250 \
                 --inter_size 4 \
                 --use_wo \
                 --language roen \
                 --universal \
-                #--debug \
-                # --load_dataset \
                 # --debug \
 
+                #--share_universal_embedding \
+                # --load_dataset \
+                # --debug \
+                # --dataset europarl_6k_fv \
 
